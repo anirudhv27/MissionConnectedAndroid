0b2c44b0b0819faac2a6c446736e2cfa9e71b733
Anirudh Valiveru <avaliveru@gmail.com>
2020-07-27 00:21:47 -0700

    Finished implementing list views

--- app/src/main/java/com/avaliveru/missionconnected/dataModels/Event.java
+++ app/src/main/java/com/avaliveru/missionconnected/dataModels/Event.java
@@ -1,9 +1,25 @@
 package com.avaliveru.missionconnected.dataModels;
 
-public class Event implements Comparable{
+import java.util.Date;
+
+public class Event implements Comparable<Event>{
+
+    public String eventName;
+    public String eventClub;
+    public String eventDescription;
+    public String eventImageURL;
+    public String eventID;
+    public String eventPreview;
+    public Date eventDate;
+    public long numberOfAttendees;
 
     @Override
-    public int compareTo(Object o) {
-        return 0;
+    public int compareTo(Event event) {
+        if (eventID.equals(event.eventID)) return 0;
+        else if (eventDate.equals(event.eventDate)){
+            return eventName.compareTo(event.eventName);
+        } else {
+            return eventDate.compareTo(event.eventDate);
+        }
     }
 }
