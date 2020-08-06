8704afe182b51ea88787afc2e7e5a7eeb6733320
Siva <Anirudh Valiveru>
2020-08-05 22:00:49 -0700

    Login restrictions for speciala nd fusd users. Alert messages

--- app/src/main/java/com/avaliveru/missionconnected/GoogleSignInActivity.java
+++ app/src/main/java/com/avaliveru/missionconnected/GoogleSignInActivity.java
@@ -1,14 +1,16 @@
 package com.avaliveru.missionconnected;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
-import com.google.android.gms.auth.api.Auth;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -17,12 +19,19 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 
 public class GoogleSignInActivity extends AppCompatActivity implements View.OnClickListener {
@@ -31,11 +40,37 @@ public class GoogleSignInActivity extends AppCompatActivity implements View.OnCl
     private static final int RC_SIGN_IN = 9001;
     private GoogleSignInClient mGoogleSignInClient;
     SignInButton signInButton;
+    FirebaseUser currentUser;
+    private ArrayList<String> domains;
+    private ArrayList<String> special_users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_sign_in);
+        domains = new ArrayList<>();
+        special_users = new ArrayList<>();
+
+        //building data for email check
+        FirebaseDatabase.getInstance().getReference()
+                .child("schools").child("missionsanjosehigh").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                DataSnapshot domainSnap = snapshot.child("domains");
+                DataSnapshot specialUsers = snapshot.child("special_users");
+                for (DataSnapshot child : domainSnap.getChildren())
+                     domains.add((String) child.getValue());
+                for (DataSnapshot child : specialUsers.getChildren())
+                    special_users.add((String) child.getValue());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        //end data to check email validity/////
+
         mAuth = FirebaseAuth.getInstance();
         if(mAuth.getCurrentUser()!= null){
             Intent newIntent = new Intent(GoogleSignInActivity.this, MainActivity.class);
@@ -46,7 +81,7 @@ public class GoogleSignInActivity extends AppCompatActivity implements View.OnCl
                     .requestEmail()
                     .build();
             mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-            signInButton = (SignInButton) findViewById(R.id.signInButton);
+            signInButton = findViewById(R.id.signInButton);
             signInButton.setOnClickListener(this);
         }
     }
@@ -56,41 +91,97 @@ public class GoogleSignInActivity extends AppCompatActivity implements View.OnCl
         if (requestCode == RC_SIGN_IN) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             try {
-                // Signin successful
-                //Log.d(TAG, "signInWithCredential:success onActivityResult");
-                GoogleSignInAccount account = task.getResult(ApiException.class);
-                firebaseAuthWithGoogle(account.getIdToken());
+               GoogleSignInAccount account = task.getResult(ApiException.class);
+                assert account != null;
+                if(isValidEmail(account.getEmail()) ){
+                    firebaseAuthWithGoogle(account.getIdToken());
+                }else
+                   alertLoginError(getString(R.string.invalid_loginid_alert));
 
             } catch (ApiException e) {
-               // Log.d(TAG, "signInWithCredential:failed "+e.getMessage());
-                //Log.d(TAG, "signInWithCredential:failed onActivityResult");
                 // Sign in failed
-                //TODO implement logic if authentication fails
+                alertLoginError(getString(R.string.general_login_failure_error));
             }
         }
     }
 
+    private boolean isValidEmail(String email) {
+        boolean validUser = false;
+        String emailDomain = email.split("@")[1];
+
+        if(special_users.contains(email) || domains.contains(emailDomain)  )
+            validUser = true;
+        return validUser;
+
+    }
+
+    private void alertLoginError(String message) {
+        AlertDialog.Builder alertDialog2 = new AlertDialog.Builder( this);
+        alertDialog2.setTitle("Alert");
+        alertDialog2.setMessage(message);
+        alertDialog2.setPositiveButton("OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                                .requestIdToken(getString(R.string.default_web_client_id))
+                                .requestEmail()
+                                .build();
+                        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(getBaseContext(), gso);
+                        mGoogleSignInClient.signOut();
+                                dialog.cancel();
+
+                    }
+                });
+        alertDialog2.show();
+    }
+
+
     private void firebaseAuthWithGoogle(String idToken) {
-        //showProgressBar();
         AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
-        Log.d(TAG, "Got Credentials");
         mAuth.signInWithCredential(credential)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            Log.d(TAG, "Calling intent ");
-                            Intent newIntent = new Intent(GoogleSignInActivity.this, MainActivity.class);
-                            startActivity(newIntent);
+                            currentUser = mAuth.getCurrentUser();
+                                addUserToFirebaseDB();
+                                Intent newIntent = new Intent(GoogleSignInActivity.this, MainActivity.class);
+                                startActivity(newIntent);
                         } else {
-                            // If sign in fails, display a message to the user.
+                            alertLoginError(getString(R.string.firebase_auth_error));
                             Log.d(TAG, "signInWithCredential:failed");
-
                         }
                     }
+
                 });
+    }
+
+
+
+    private void addUserToFirebaseDB() {
+        FirebaseDatabase.getInstance().getReference().child("users")
+                    .child(currentUser.getUid()).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if(!snapshot.exists()) {
+                        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
+                        DatabaseReference usersRef = dbRef.child("users");
+
+                        String key = currentUser.getUid();
+                        usersRef.child(key).child("email").setValue(currentUser.getEmail());
+                        usersRef.child(key).child("fullname").setValue(currentUser.getDisplayName());
+                        usersRef.child(key).child("isAdmin").setValue(false);
+                        usersRef.child(key).child("school").setValue("missionsanjosehigh");
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                            usersRef.child(key).child("imgurl").setValue(Objects.requireNonNull(currentUser.getPhotoUrl()).toString());
+                        }
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) { }
+            });
+
     }
 
     @Override
